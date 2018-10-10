d2a53bde0bfe8678313946e8e0bd4df705d87974
VENTURA Simone <simone.ventura@soprasteria.com>
2018-10-10 14:27:19 +0200

    Fixed map

--- locator.js
+++ locator.js
@@ -1,27 +1,21 @@
-/*$.getScript("ipLookup.js");
-$.getScript("map.js");*/
-
-var div = $("#map"),
+var div = "#map",
             zoom = 5,
-            width = "100%",
-            height = "50%";
+            width = "50%",
+            height = "100%",
+            lat,
+            lng;
 
         RestModule.callRestService(function(json){
             ip = json.ip;
             lat = json.latitude;
             lng = json.longitude;
 
-        $("#ip").text(ip);
-        $("#json").text(JSON.stringify(json,null,2));
-        });
-        console.log(width);
-        /*map = MapHandler.createMap(div,43.222897,6.8444381,zoom,width,height);*/
-        var map = new GMaps({
-            div:$(".col-md-6#map"),
-            lat:'43.222897',
-            lng:'6.8444381',
-            zoom: zoom,
-            width: width,
-            height: height
+            $("#ip").text(ip);
+            $("#json").text(JSON.stringify(json,null,2));
+
+            map = MapHandler.createMap(div,43.222897,6.8444381,zoom,width,height);
+            map.addMarker({
+                lat,lng
             });
-        map.addMaker(lat,lng);
+        });
+        
